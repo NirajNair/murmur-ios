ccc888cd052fb8978290f121a6ab8f0cd8f5fe1e
Niraj Nair <nirajnknair@gmail.com>
2025-08-12 00:35:21 +0530

    Initial Commit

---
+++ MurMur/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MurMur
+//
+//  Created by Niraj Nair on 12/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MurMur/MurMurApp.swift
@@ -0,0 +1,17 @@
+//
+//  MurMurApp.swift
+//  MurMur
+//
+//  Created by Niraj Nair on 12/08/25.
+//
+
+import SwiftUI
+
+@main
+struct MurMurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
